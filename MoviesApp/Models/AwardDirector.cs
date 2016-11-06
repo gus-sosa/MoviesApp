b930d6dfe2cb140183f89af40eaba4445e5a20e8
Gustavo Sosa <g.sosacuervo1@gmail.com>
2016-11-06 12:36:28 -0600

    Model Director added

---
+++ MoviesApp/Models/AwardDirector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesApp.Models
+{
+    public class AwardDirector
+    {
+        public int ID { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Category { get; set; }
+
+        public virtual Director Director { get; set; }
+
+        public virtual Award Award { get; set; }
+    }
+}
---
+++ MoviesApp/Models/Director.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesApp.Models
+{
+    public class Director
+    {
+        public int DirectorID { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string Biography { get; set; }
+
+        public virtual IEnumerable<Movie> Movies { get; set; }
+
+        public virtual IEnumerable<AwardDirector> Awards { get; set; }
+    }
+}
